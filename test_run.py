04b180edab3507ba94f18d2089b65cfba095e7e5
andrewnnov <andrewnnov@yandex.ru>
2021-10-13 09:52:03 +0300

    create selectors for playwright app

--- test_run.py
+++ test_run.py
@@ -6,34 +6,43 @@ from playwright.sync_api import Playwright, sync_playwright
 def run(playwright: Playwright) -> None:
 
     # Assess - Given
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     # Open new page
     page = context.new_page()
-    # Go to https://symonstorozhenko.wixsite.com/website-1
+
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.set_default_timeout(3000)
 
     # Act - When/And
     # Click button:has-text("Log In")
-    # time.sleep(5)
-    page.click("button:has-text(\"Log In\")", timeout=7000)
-    # Click [data-testid="signUp.switchToSignUp"]
-    # time.sleep(5)
+
+    login_issue = True
+    while login_issue:
+        if not page.is_visible("[data-testid=\"signUp.switchToSignUp\"]"):
+            page.click("button:has-text(\"Log In\")")
+        else:
+            login_issue = False
+        page.wait_for_timeout(1000)
+    # page.wait_for_selector("button:has-text(\"Log In\")")
+    # page.click("button:has-text(\"Log In\")")
+    # page.click("text=Log In")
+    # page.click("'Log In'")
+
     page.click("[data-testid=\"signUp.switchToSignUp\"]")
-    # Click [data-testid="switchToEmailLink"] [data-testid="buttonElement"]
+
     page.click("[data-testid=\"switchToEmailLink\"] [data-testid=\"buttonElement\"]")
-    # Click [data-testid="siteMembers.container"] input[type="email"]
+
     page.click("[data-testid=\"siteMembers.container\"] input[type=\"email\"]")
-    # Fill [data-testid="siteMembers.container"] input[type="email"]
+
     page.fill("[data-testid=\"siteMembers.container\"] input[type=\"email\"]", "test@testmail.com")
-    # Click input[type="password"]
+
     page.click("input[type=\"password\"]")
-    # Fill input[type="password"]
+
     page.fill("input[type=\"password\"]", "test123")
-    # Click [data-testid="submit"] [data-testid="buttonElement"]
+
     page.click("[data-testid=\"submit\"] [data-testid=\"buttonElement\"]")
-    # Click [aria-label="test account menu"]
+
     page.click("[aria-label=\"test account menu\"]")
 
     # Assert - Then
@@ -43,8 +52,8 @@ def run(playwright: Playwright) -> None:
     # with page.expect_navigation():
     #     page.click("text=My Orders")
     # ---------------------
-    context.close()
-    browser.close()
+    # context.close()
+    # browser.close()
 
 
 with sync_playwright() as playwright:
